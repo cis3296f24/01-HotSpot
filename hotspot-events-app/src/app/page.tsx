f3b21d1a49d48743c9dd25aedca5731c9227a0f5
Hxrob <tun85603@temple.edu>
2024-11-08 02:50:43 -0500

    Added profile page

--- hotspot-events-app/src/app/page.tsx
+++ hotspot-events-app/src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   const [isRegistered, setIsRegistered] = useState(false);
   const router = useRouter();
 
-
   const [formData, setFormData] = useState({
     eventName: "",
     eventDate: "",
